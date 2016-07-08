5ffc7c413e70ab7eaf8ed24182ea1429cf1efe3b
nlistes <nlistes@X1-PC>
2016-07-08 22:54:31 +0300

    Project to test Danfos valve motor

---
+++ HardwareTesting/DanfosMotorControl/DanfosMotorControl.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		DanfosMotorControl.ino
+ Created:	7/8/2016 10:52:26 PM
+ Author:	Lietotajs
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+}
+
+// the loop function runs over and over again until power down or reset
+void loop()
+{
+}
