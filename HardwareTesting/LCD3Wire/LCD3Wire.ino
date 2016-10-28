0a514271a26c946ea6c084bc2196406dd0d84cff
Lietotajs <Lietotajs@X1-PC>
2016-10-29 01:57:13 +0300

    2x16 LCD display example with 3 wires

---
+++ HardwareTesting/LCD3Wire/LCD3Wire.ino
@@ -0,0 +1,27 @@
+/*
+ Name:		LCD3Wire.ino
+ Created:	10/29/2016 1:42:07 AM
+ Author:	nlistes
+*/
+
+#include <LiquidCrystal_SR_LCD3.h>
+
+LiquidCrystal_SR_LCD3 lcd(A2, A0, A1);
+
+int c = 0;
+
+
+void setup()
+{
+	lcd.begin(16, 2);        // set up the LCD's number of columns and rows
+	lcd.print("Aiziet...");
+	lcd.setCursor(0, 1);
+}
+
+void loop()
+{
+	c++;
+	lcd.print(c);
+	delay(1000);
+	lcd.setCursor(0, 1);
+}
